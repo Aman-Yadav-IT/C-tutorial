8427fa2200813dcfa79b738bc53b342286a941e2
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-06 01:28:06 +0530

    rename

--- tutorail18.c
+++ tutorail18.c
@@ -5,6 +5,11 @@ float b = (float)54/5;
 printf("value of b is : %f\n",b);
 printf("value of a is : %d\n",a);
 return 0;
+
 }
+
 // type casting is used to convert one data type into another data type. 
-// In this example, integer and interer divison always gives integer value but if we want to get float value then we have to type cast one of the variable into float. In this example we have type casted 54 into float so the result will be float value.
+// In this example, integer and interer divison always gives integer value but if we want to get float value then we have to type cast one of the variable into float.
+//  In this example we have type casted 54 into float so the result will be float value.
+
+   
