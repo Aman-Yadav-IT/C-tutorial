9f0ed1c0b1b2de69b385a58229a1f13edcdfc3f0
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-09 01:04:20 +0530

    pratice

---
+++ allbasics.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+ /* we use #include <stdio.h> to include the standard input output library in our program
+ which is used to take input from the user and print output to the screen. 
+ */
+
+
+ /* where int main () is the main function of the program where the execution of the program starts. 
+ without int main() function the program will not run.  
+*/
+
+/* %d is used to print the integer value which is already defined  */
+/* scanf is used to take input from the user */
+/* now scanf("%d",&age); this is used to read an integer value from the user and store it in the variable age
+where %d is the format specifier for integer and &age is the address of the variable age 
+the &age tell the compiler to store the value at the memory address of the variable age 
+this help to access and modify the value of the variable age  without &age the value of the variable age cannot be modified*/
+
--- quiz2tutorial10.c
+++ quiz2tutorial10.c
@@ -3,12 +3,19 @@
 int main()
 {
     char s[20];
-    int subject;
+    /* here char is used to declare a character array to store the input string 
+    where s is a placeholder for the string */
+
+    
     printf("which subject you have pass maths science or both :\n");
     scanf("%19s",s);
 printf("your subject is : %s\n",s);
 // we use == for comparision operator and = for assignment operator example for == in simple word is equal to and = is used to assign a value to a variable
 if (strcmp(s,"maths")==0)
+/* here strcmp is used to compare two strings */
+/* What you wrote:if ( strcmp(s, "maths") == 0 )Step 1 (The computer solves the function):If the words match,
+ the computer calculates that strcmp(s, "maths") equals 0.
+ Step 2 (The computer substitutes the result):The computer drops that 0 right into the spot where the function used to be. */
 {printf("you won a fidjet spinner\n");}
 else if (strcmp(s,"science")==0)
 {printf("you won a science prize\n");}
