2e4ad81a831008f5d0137e13973336a565445ad1
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-09 23:37:19 +0530

    basics

--- allbasics.c
+++ allbasics.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
-
+{
  /* we use #include <stdio.h> to include the standard input output library in our program
  which is used to take input from the user and print output to the screen. 
  */
@@ -16,4 +16,6 @@ int main()
 where %d is the format specifier for integer and &age is the address of the variable age 
 the &age tell the compiler to store the value at the memory address of the variable age 
 this help to access and modify the value of the variable age  without &age the value of the variable age cannot be modified*/
-
+b=15.83563
+printf("%23.4f,b")
+}
