8bdbd69d947f6e9d0867b85f5602bc7872fb23aa
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-11 18:40:54 +0530

    user defined functions

--- tutorial19.c
+++ tutorial19.c
@@ -1,25 +1,24 @@
-#include <stdio.h.>
-int main()
-{
-printf("hello world");
+#include <stdio.h>
 
-/*the basic syntax of c funtion is as follows
-return_type funtion_name(data_type parameter 1,data_type parameter2,...)
-//code to be execute */
-
-/*declaration,definaton and call in funtion 
-1. A funtion is declared to tell a coumputer about its existence
-2. A funtion is diefined to get some task done (actual impeltion is done here)
-3. A funtion is called in order to be used */
-/*types of funtions 
-1.libary funtion-funtion included in c header files
-2.user defined funtion-funtion created by c programmer 
-to reduce complexity of a program 
-*/
-int a,b,c;
-a = 9;
-b = 87;
-c = sum(a, b);
-
-
+// 1. Define the sum function OUTSIDE of main
+int sum(int a, int b) {
+    return a + b;
 }
+
+int main() { // 2. Open the main function here
+    printf("hello world\n");
+    
+    /* your comments can go here */
+    
+    int a, b, c;
+    a = 9;
+    b = 87;
+    
+    // 3. Call the sum function
+    c = sum(a, b); 
+    
+    printf("The sum is: %d\n", c);
+    
+    return 0;
+} // 4. Close the main function here
+
