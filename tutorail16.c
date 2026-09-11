9ac2641804f9b6db743e588438adcc8428b98ee3
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-11 18:31:02 +0530

    user defind funtion

--- tutorail16.c
+++ tutorail16.c
@@ -18,7 +18,7 @@ for (i=0; i<10; i++)
     printf("now this will get printed\n");
 
 // so today we have learned about break and continue statement in c programming language. break statement is used to terminate the loop and continue statement is used to skip the current iteration of the loop and move to the next iteration of the loop.
-
+// 
 
 
     }
---
+++ tutorial19.c
@@ -0,0 +1,21 @@
+#include <stdio.h.>
+int main()
+{
+printf("hello world");
+
+/*the basic syntax of c funtion is as follows
+return_type funtion_name(data_type parameter 1,data_type parameter2,...)
+//code to be execute */
+
+/*declaration,definaton and call in funtion 
+1. A funtion is declared to tell a coumputer about its existence
+2. A funtion is diefined to get some task done (actual impeltion is done here)
+3. A funtion is called in order to be used */
+/*types of funtions 
+1.libary funtion-funtion included in c header files
+2.user defined funtion-funtion created by c programmer 
+to reduce complexity of a program 
+*/
+
+
+}
