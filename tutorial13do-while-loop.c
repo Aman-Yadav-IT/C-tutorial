5a67a5325ebd7d0d160bcb1555faae9736b52d09
Aman-Yadav-IT <amnydv44@gmail.com>
2026-09-13 18:52:59 +0530

    do while

--- tutorial13do-while-loop.c
+++ tutorial13do-while-loop.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-    int num, index=4;
+    int num, index = 0; // Creates 'num' for user input, and 'index' to count from 0
+
     printf("Enter a number: \n");
-    scanf("%d", &num);
-   
-    do {
-        printf("%d\n" , index+1);
-        index++;
+    scanf("%d", &num); // Saves the user's input limit the value of num (e.g., 5)
 
+    // 🔄 DO-WHILE LOOP: Runs the code first, checks the condition later
+    do
+    {
+        printf("%d\n", index + 1); // Prints (index + 1) so counting starts visually at 1 like 0+1 then 2 then 3 until index become less than num 
+        index++;                   // Adds 1 to the counter so the loop moves forward
 
-    
-    }while(index<num);
-
-    
-
-
-
-
-
-
-
-
+    } while (index < num); // Checkpoint: Jumps back up to 'do' ONLY if index is smaller than num. 
+ return 0;
 }
